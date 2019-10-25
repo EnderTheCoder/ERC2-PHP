025a401986ca1c45ea36beea358b35f342f92bae
EnderTheCoder <47713889+EnderTheCoder@users.noreply.github.com>
2019-10-25 08:18:02 +0800

    Update mysql_config.php
    SAFE

--- config/mysql_config.php
+++ config/mysql_config.php
@@ -1,5 +1,5 @@
 <?php
-define("MYSQL_HOST", "frp.yishugou.shop", true);
+define("MYSQL_HOST", "LOCALHOST", true);
 define("MYSQL_DB_NAME", "ERC2", true);
 define("MYSQL_USER_NAME", "Ender", true);
-define("MYSQL_PASSWORD", "Feng,HK,4778!", true);
+define("MYSQL_PASSWORD", "123456", true);
